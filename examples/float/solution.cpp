109597109af4ed22cfa28d5f8d1a15b05a4f29d5
Raul Oliveira <craulg9@gmail.com>
2025-05-07 23:57:56 -0300

    1.1.0

---
+++ examples/float/solution.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    float a, b;
+    cin >> a >> b;
+    float sum = a + b;
+    cout << fixed << setprecision(4) << sum << endl;
+}
--- main.py
+++ main.py
@@ -4,28 +4,31 @@ import shutil
 import os
 import os.path
 import string
+import time
 
 SOLUTION_FILENAME = "solution.cpp"
 INPUT_FILENAME = "input.txt" # You can change these
 
-def log(content):
-    print(f"[TestGenCpp] {content}")
+def log(content, error=False):
+    print(f"{"\033[91mERROR -" if error else f"-"} {content}")
 
 def main(num_test_cases):
+    time_start = time.time()
+
     if not os.path.isfile(f"./{SOLUTION_FILENAME}"):
-        log(f"Could not find the solution `{SOLUTION_FILENAME}`!")
+        log(f"Could not find the solution `{SOLUTION_FILENAME}`!", True)
         return
     
     log(f"Solution `{SOLUTION_FILENAME}` found!")
 
     if not os.path.isfile(f"./{INPUT_FILENAME}"):
-        log(f"Could not find the input template `{INPUT_FILENAME}`!")
+        log(f"Could not find the input template `{INPUT_FILENAME}`!", True)
         return
     
     log(f"Input template `{INPUT_FILENAME}` found!")
 
     log("Compiling solution...")
-    subprocess.run(f"g++ {SOLUTION_FILENAME} -o .solution", shell=True, executable="/bin/bash")
+    subprocess.run(f"g++ {SOLUTION_FILENAME} -o _temp_solution", shell=True, executable="/bin/bash")
     log("Solution compiled successfully!")
 
     if os.path.exists("./cases"):
@@ -45,12 +48,29 @@ def main(num_test_cases):
 
         for arg in modelo_args:
             if arg[1] == "i":
-                range_min, range_max = [int(x) for x in arg[3:-1].split("-")]
+                range_min, range_max = [int(x) for x in arg[3:-1].split(":")]
+
+                if range_max < range_min:
+                    log(f"Cannot generate a random integer because {range_min} > {range_max}", True)
+                    return
+                
                 final_input = final_input.replace(arg, f"{random.randint(range_min, range_max)}", 1)
 
             if arg[1] == "f":
-                range_min, range_max = [float(x) for x in arg[3:-1].split("-")]
-                final_input = final_input.replace(arg, f"{random.uniform(range_min, range_max)}", 1)
+                options = arg[3:-1].split(":")
+                
+                range_min, range_max = map(float, options[:2])
+
+                float_precision = 5
+
+                if len(options) == 3:
+                    float_precision = int(options[2])
+
+                if range_max < range_min:
+                    log(f"Cannot generate a random float because {range_min} > {range_max}", True)
+                    return
+                
+                final_input = final_input.replace(arg, f"{round(random.uniform(range_min, range_max), float_precision)}", 1)
 
             if arg[1] == "s":
                 options = arg[3:-1].split(":")
@@ -89,14 +109,19 @@ def main(num_test_cases):
         log(f"Generating `{i}.out`...")
 
         with open(f"./cases/{i}.out", "w") as file:
-            subprocess.run(f"(cat ./cases/{i}.in | ./.solution) >> ./cases/{i}.out", shell=True, executable="/bin/bash")
+            subprocess.run(f"(cat ./cases/{i}.in | ./_temp_solution) >> ./cases/{i}.out", shell=True, executable="/bin/bash")
 
-    os.remove("./.solution")
+    os.remove("./_temp_solution")
     log(f"Removed compiled solution.")
 
-    log(f"{num_test_cases} test cases generated successfully! ({os.getcwd()}/cases)")
+    time_end = time.time()
+
+    log(f"Successfully generated {num_test_cases} test cases in {time_end-time_start:.5f}s! ({os.getcwd()}/cases)")
 
 if __name__ == "__main__":
     num_test_cases = int(input("Enter the number of test cases to generate: "))
-    
-    main(num_test_cases)
+
+    if num_test_cases <= 0:
+        log("Invalid number of test cases.", True)
+    else:
+        main(num_test_cases)
